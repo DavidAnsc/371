955d5cec9039adf50339ffdefea2f3b8225855d6
David An <ansichen.david@gmail.com>
2026-08-22 15:11:45 +0800

    dev

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 // stage: 0, 1, 2
 
-static std::array<std::array<Block, 5>, 12> coordinate {{{Block(), Block(), Block({4, 5, 6}), Block({4, 5, 6}), Block()},
+static std::array<std::array<Block, 5>, 12> coordinate {{
+                                                  {Block(), Block(), Block({4, 5, 6}), Block({4, 5, 6}), Block()},
                                                   {Block(), Block(), Block({4, 5, 6}), Block(), Block()},
                                                   {Block(), Block(), Block(), Block(), Block()},
                                                   {Block(), Block(), Block(), Block(), Block()},
@@ -18,7 +19,8 @@ static std::array<std::array<Block, 5>, 12> coordinate {{{Block(), Block(), Bloc
                                                   {Block(), Block(), Block(), Block(), Block()},
                                                 }};
 
-static std::array<std::array<int, 5>, 12> orgMarks {{{0, 0, 1, 1, 0}, 
+static std::array<std::array<int, 5>, 12> orgMarks {{
+                                              {0, 0, 1, 1, 0}, 
                                               {0, 0, 1, 0, 0}, 
                                               {0, 0, 0, 0, 0}, 
                                               {0, 0, 0, 0, 0},
@@ -31,6 +33,29 @@ static std::array<std::array<int, 5>, 12> orgMarks {{{0, 0, 1, 1, 0},
                                               {0, 0, 1, 0, 0},
                                               {0, 0, 0, 0, 0}}};
 
+bool checkIsShaded(int x, int y) {
+  if (
+    (x != 0 && y != 1) &&
+    (x != 1 && y != 1) &&
+    (x != 1 && y != 3) &&
+    (x != 2 && y != 3) &&
+    (x != 4 && y != 1) &&
+    (x != 4 && y != 2) &&
+    (x != 4 && y != 3) &&
+    (x != 6 && y != 1) &&
+    (x != 7 && y != 1) &&
+    (x != 7 && y != 3) &&
+    (x != 8 && y != 3) &&
+    (x != 9 && y != 3) &&
+    (x != 10 && y != 3) &&
+    (x != 10 && y != 1) &&
+    (x != 11 && y != 1)
+  ) {
+    return false;
+  }
+  return true;
+}
+
 void recurse(int xOld, int yOld, int choice, int stage, std::array<std::array<Block, 5>, 12> coordinateSystem) {
   if (stage == 0) {
     const Block tempBlock = coordinateSystem.at(xOld).at(yOld);
@@ -40,32 +65,35 @@ void recurse(int xOld, int yOld, int choice, int stage, std::array<std::array<Bl
       return;
     }
     
-    if (orgMarks.at(xOld).at(yOld) != 0) {
+    int nextY = yOld;
+    int nextX = xOld;
+
+    if (nextY == 4) {
+      ++nextX;
+      nextY = 0;
+    } else {
+      ++nextY;
+    }
+
+    if (nextX >= 12) {
+      printf("you've finished stage 0 compute.\n");
+      return;
+    }
+    
+    if (orgMarks.at(xOld).at(yOld) != 0 || checkIsShaded(xOld, yOld) == true) {
       coordinateSystem.at(xOld).at(yOld).setValue(choice);
       printf("Assigned value: [%d] to (%d, %d)\n", choice, xOld, yOld);
     }
 
-    if (yOld == 4) {
-      ++xOld;
-      yOld = 0;
-    } else {
-      ++yOld;
-    }
-
-    if (xOld >= 12) {
-      printf("you've finished stage 0 compute.\n");
-      return;
-    }
-
-
-    for (int x = xOld; x < 12; ++x) {
-      for (int y = yOld; y < 5; ++y) {
+    for (int x = nextX; x < 12; ++x) {
+      for (int y = nextY; y < 5; ++y) {
         if (x > static_cast<size_t>(std::numeric_limits<int>::max()) || y > static_cast<size_t>(std::numeric_limits<int>::max())) {
           printf("the value of x or y exceeds the integer range. continuing...");
           continue;
         }
-        for (size_t i = 0; i < coordinateSystem.at(x).at(y).getLength(); ++i) {
-          recurse(x, y, coordinateSystem.at(x).at(y).getPossibleValues().at(i), 0, coordinateSystem);
+        const auto newBlock = coordinateSystem.at(x).at(y);
+        for (int i = 0; i < newBlock.getLength(); ++i) {
+          recurse(x, y, newBlock.getPossibleValues().at(i), 0, coordinateSystem);
         }
       }
     }
